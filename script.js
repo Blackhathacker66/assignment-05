27d96d7a49136f19f191522e2fdc73329a3bb88a
Blackhathaker66 <blackhathaker66@gmail.com>
2025-08-28 14:50:20 +0600

    Call update feature in js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // available icon function
-function availAbleCoin(id){
+function availAbleCoin(id) {
     const coin = document.getElementById(id).innerText;
     const coinNumber = parseInt(coin)
     if (coinNumber < 20) {
@@ -11,4 +11,26 @@ function availAbleCoin(id){
     }
     const upDateAvailableCoin = coinNumber - 20;
     document.getElementById(id).innerText = upDateAvailableCoin;
+}
+
+
+// Call History Update 
+function updateCallHistory(title, number) {
+    const time = getTime();
+
+    const newCard = document.createElement('div');
+    newCard.className = "flex justify-between items-center bg-[#FAFAFA] p-1 rounded-lg mb-5";
+
+    newCard.innerHTML = `
+                        <div>
+                            <h1 class="font-bold text-lg">${title}</h1>
+                            <p  class="text-[16px]">${number}</p>
+                        </div>
+                        <div>
+                            <p class="text-[16px]">${time}</p>
+                        </div>
+            `;
+
+    const historyContainer = document.getElementById('history-container');
+    historyContainer.appendChild(newCard);
 }
